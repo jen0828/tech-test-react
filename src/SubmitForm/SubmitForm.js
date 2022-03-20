688dacde264e5639ac860e16b3a18e43ca36d58a
Jenny Moon <jenny0moon@gmail.com>
2022-03-20 12:02:15 +0000

    Add functionality: save user input info to database using axios post request

--- src/SubmitForm/SubmitForm.js
+++ src/SubmitForm/SubmitForm.js
@@ -1,6 +1,43 @@
 import { Form, Button, Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function SubmitForm() {
+  const [title, setTitle] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [dob, setDob] = useState('');
+  const [income, setIncome] = useState('');
+  const [employment, setEmployment] = useState('');
+  const [houseNumber, setHouseNumber] = useState('');
+  const [postcode, setPostcode] = useState('');
+
+  const api_url = 'http://localhost:8000/users';
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post(api_url, {
+        title: title,
+        firstName: firstName,
+        lastName: lastName,
+        dob: dob,
+        income: income,
+        employment: employment,
+        houseNumber: houseNumber,
+        postcode: postcode,
+      })
+      .then((response) => {
+        const currentCustomer = JSON.stringify(response.data);
+        // console.log(response.data);
+        console.log(currentCustomer);
+      })
+      .catch((error) => {
+        console.log('Something went wrong!', error);
+      });
+  };
+
   return (
     <>
       <div className="d-flex justify-content-center mt-5">
@@ -9,24 +46,41 @@ export default function SubmitForm() {
           className="shadow p-3 mb-5 bg-body rounded">
           <Card.Body>
             <Card.Title>Find the right credit card for you</Card.Title>
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Title</Form.Label>
-                <Form.Control type="title" required placeholder="Mr" />
+                <Form.Control
+                  type="title"
+                  required
+                  placeholder="Mr"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>First Name</Form.Label>
-                <Form.Control type="firstName" required placeholder="Ollie" />
+                <Form.Control
+                  type="firstName"
+                  required
+                  placeholder="Ollie"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control type="lastName" required placeholder="Murphree" />
+                <Form.Control
+                  type="lastName"
+                  required
+                  placeholder="Murphree"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>DOB</Form.Label>
-                <Form.Control type="dob" required placeholder="01/07/1970" />
+                <Form.Control type="dob" required placeholder="01/07/1970"
+                onChange={(e) => setDob(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -35,22 +89,38 @@ export default function SubmitForm() {
                   type="employment"
                   required
                   placeholder="Full-time"
+                  onChange={(e) => setEmployment(e.target.value)}
                 />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Income</Form.Label>
-                <Form.Control type="income" required placeholder="34000" />
+                <Form.Control
+                  type="income"
+                  required
+                  placeholder="34000"
+                  onChange={(e) => setIncome(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>House Number</Form.Label>
-                <Form.Control type="houseNumber" required placeholder="700" />
+                <Form.Control
+                  type="houseNumber"
+                  required
+                  placeholder="700"
+                  onChange={(e) => setHouseNumber(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Postcode</Form.Label>
-                <Form.Control type="postcode" required placeholder="BS14 9PR" />
+                <Form.Control
+                  type="postcode"
+                  required
+                  placeholder="BS14 9PR"
+                  onChange={(e) => setPostcode(e.target.value)}
+                />
               </Form.Group>
 
               <div className="d-flex justify-content-around">
