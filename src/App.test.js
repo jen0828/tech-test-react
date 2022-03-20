15bb2c5fbb29d642a4a77b3bb6521a7861f4c486
Jenny Moon <jenny0moon@gmail.com>
2022-03-20 11:04:14 +0000

    Test: renders app component

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/submit form/i);
+  const linkElement = screen.getByText(/Find the right credit card for you/i);
   expect(linkElement).toBeInTheDocument();
 });
