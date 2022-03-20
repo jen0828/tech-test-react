db20fee2b6b1df653bcf2e9e44b1c59fa3d4a4d5
Jenny Moon <jenny0moon@gmail.com>
2022-03-20 11:05:15 +0000

    Test: renders app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component', () => {
   render(<App />);
   const linkElement = screen.getByText(/Find the right credit card for you/i);
   expect(linkElement).toBeInTheDocument();
