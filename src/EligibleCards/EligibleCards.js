f4d1ae114b597e296da8552b728229cea28c6740
Jenny Moon <jenny0moon@gmail.com>
2022-03-20 14:43:56 +0000

    Fetch data from cards API using axios get request

--- src/EligibleCards/EligibleCards.js
+++ src/EligibleCards/EligibleCards.js
@@ -1,9 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function EligibleCards() {
+  const [eligibleCards, setEligibleCards] = useState([]);
+  const api_url = `http://localhost:8000/cards`;
   const retrievedObject = localStorage.getItem('current customer');
   const currentCustomer = JSON.parse(retrievedObject);
 
-  console.log('current customer');
-  console.log(currentCustomer);
+
+  useEffect(() => {
+    getEligibleCards();
+    // eslint-disable-next-line
+  }, []);
+
+  const getEligibleCards = async () => {
+    try {
+      const res = await axios.get(api_url);
+      const allCards = res.data;
+      //console.log(res);
+
+      const Cards = [];
+      const Student = allCards[0];
+      const Anywhere = allCards[1];
+      const Liquid = allCards[2];
+
+      if (currentCustomer) {
+        Cards.push(Anywhere);
+      }
+      if (currentCustomer.employment === 'Student') {
+        Cards.push(Student);
+      }
+      if (currentCustomer.income > 16000) {
+        Cards.push(Liquid);
+      }
+      setEligibleCards(Cards);
+      //console.log(Cards);
+      return Cards;
+    } catch (error) {
+      console.log('Something went wrong!', error);
+    }
+  };
+
   return (
     <div>
       <h1>
