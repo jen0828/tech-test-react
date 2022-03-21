3d1cae9ad667fc4cc2de7c869dba7c956e7e22fe
Jenny Moon <jenny0moon@gmail.com>
2022-03-21 10:23:52 +0000

    Fix typo

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Find the right credit card for you/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Find the right credit card for you/i);
+  expect(textElement).toBeInTheDocument();
 });
